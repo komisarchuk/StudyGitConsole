8c5ef27af46dd102b602de4e9defa07409eef4d2
komisarchuk <komisarchuk@gmail.com>
2019-09-29 10:56:39 +0300

    Hello world

--- TextSalutSemen/Program.cs
+++ TextSalutSemen/Program.cs
@@ -10,6 +10,8 @@ namespace TextSalutSemen
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
